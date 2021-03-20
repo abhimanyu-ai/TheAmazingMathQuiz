43082e3e952efcc0434a1043d648cb208c84bcb8
Abhimanyu Krishna <74185786+abhimanyu-ai@users.noreply.github.com>
2021-03-20 18:37:22 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -1,4 +1,14 @@
-function send() {
+player_1=localStorage.getItem("player1_name");
+player_2=localStorage.getItem("player2_name");
+player1_score=0;
+player2_score=0;
+document.getElementById('player1_name').innerHTML=player_1 + ":";
+document.getElementById('player2_name').innerHTML=player_2 + ":";
+document.getElementById('player1_score').innerHTML=player1_score;
+document.getElementById('player2_score').innerHTML=player2_score;
+document.getElementById('player_question').innerHTML="Question Turn - " + player_1;
+document.getElementById('player_answer').innerHTML="Answer Turn - " + player_2;
+ function send() {
     number1=document.getElementById('number1').value;
     number2=document.getElementById('number2').value;
     actual_answer=parseInt(number1) * parseInt(number2);
@@ -9,4 +19,35 @@ function send() {
     document.getElementById('output').innerHTML=row;
     document.getElementById('number1').value="";
     document.getElementById('number2').value="";
+ }
+ question_turn="player_1";
+ answer_turn="player_2";
+ function check() {
+     get_answer=document.getElementById('input_check_box').value;
+     if(get_answer == actual_answer) {
+         if(answer_turn == "player_1") {
+            update_player1_score=player1_score + 1;
+            document.getElementById('player1_score').innerHTML=update_player1_score;
+         }
+         else {
+             update_player2_score=player2_score + 1;
+             document.getElementById('player2_score').innerHTML=update_player2_score;
+         }
+     }
+     if(question_turn == "player_1") {
+         question_turn="player_2";
+         document.getElementById('player_question').innerHTML="Question Turn - " + player_2;
+     }
+     else {
+         question_turn="player_1";
+         document.getElementById('player_question').innerHTML="Question Turn - " + player_1;
+     }
+     if(answer_turn == "player_1") {
+         answer_turn="player_2";
+         document.getElementById('player_answer').innerHTML="Answer Turn - " + player_2;
+     }
+     else {
+         answer_turn="player_1";
+         document.getElementById('player_answer').innerHTML="Answer Turn - " + player_1;
+     }
  }
